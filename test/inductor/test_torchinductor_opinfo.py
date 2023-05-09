3583b2354ed607e1892343f44940a55849fa7fec
Jack Taylor <jack.taylor@amd.com>
2023-05-09 16:14:11 +0000

    Disable some UT suites on ROCm to ease upstream transition

--- test/inductor/test_torchinductor_opinfo.py
+++ test/inductor/test_torchinductor_opinfo.py
@@ -260,6 +260,7 @@ inductor_expected_failures_single_sample["cpu"] = {
     "complex": {f16, f32, f64},
 }
 
+
 inductor_expected_failures_single_sample["cuda"] = {
     "__getitem__": {b8, f16, f32, f64, i32, i64},
     "__rdiv__": {b8, f16, f32, f64, i32, i64},
@@ -327,10 +328,6 @@ inductor_expected_failures_single_sample["cuda"] = {
     "unique_consecutive": {b8, f16, f32, f64, i32, i64},
     # AssertionError: Tensor-likes are not close!
     "nn.functional.triplet_margin_loss": {f16},
-    # The following 3 tests fail on CUDA with AssertionError: expected size 5==5, stride 5==1 at dim=0
-    # linalg._svd's return value has different strides on CUDA vs CPU which causes this
-    # In test_meta.py there is a mechanism to skipping strides checks for some ops
-    # (including _linalg_svd), possibly we should have something similar here
     "linalg.matrix_rank": {f32, f64},
     "linalg.svd": {f32, f64},
     "pca_lowrank": {f32, f64},
@@ -364,13 +361,12 @@ inductor_expected_failures_single_sample["cuda"] = {
 }
 
 if not TEST_WITH_ROCM:
-    # The following 3 tests fail on CUDA with AssertionError: expected size 5==5, stride 5==1 at dim=0
+    # The following tests fail on CUDA with AssertionError: expected size 5==5, stride 5==1 at dim=0
     # linalg._svd's return value has different strides on CUDA vs CPU which causes this
     # In test_meta.py there is a mechanism to skipping strides checks for some ops
     # (including _linalg_svd), possibly we should have something similar here
     inductor_expected_failures_single_sample["cuda"]["linalg.cond"] = {f32, f64}
     inductor_expected_failures_single_sample["cuda"]["linalg.svdvals"] = {f32, f64}
-    inductor_expected_failures_single_sample["cuda"][("norm", "nuc")] = {f32, f64}
 
 inductor_gradient_expected_failures_single_sample = defaultdict(dict)
 
